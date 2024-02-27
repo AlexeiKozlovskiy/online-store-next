886d0d4e5b8a77764707d3f5bf456fe2093680d7
AlexeiKozlovskiy <gekleberefin@gmail.com>
2024-02-27 17:36:24 +0300

    feat: configure workflow, based on #3

--- my-app/test/index.test.tsx
+++ my-app/test/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders home page', () => {
     const { getByText } = render(<Home />);
 
     const descriptionText = getByText('Get started by editing');
