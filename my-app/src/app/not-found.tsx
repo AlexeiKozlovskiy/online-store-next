f4a5a2f756b22b9955a3b8c8a98e2fec2e5f9eda
AlexeiKozlovskiy <gekleberefin@gmail.com>
2024-02-28 21:13:49 +0300

    feat: 404 page, based on #6

---
+++ my-app/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styles from '@/styles/not-found.module.scss';
+
+export default function Custom404() {
+  return (
+    <div className={styles.page404__container}>
+      <p>Whoops!</p>
+      <p>Looks like you got lost.</p>
+      <p>
+        {`Go back to the `}
+        <Link className={styles.page404__link} href="/">
+          home
+        </Link>
+        {` page?`}
+      </p>
+    </div>
+  );
+}
