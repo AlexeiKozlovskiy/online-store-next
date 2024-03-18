4f1c91f717fbacb0ac0b44c7d706f9f0367bd2c9
AlexeiKozlovskiy <gekleberefin@gmail.com>
2024-03-18 12:32:30 +0300

    fix: routing vercel, based on #8

--- my-app/src/app/product/[productId]/page.tsx
+++ my-app/src/app/product/[productId]/page.tsx
@@ -1,3 +1,3 @@
-export default function Product({ params }: any) {
+export default function Product({ params }: { params: { productId: string } }) {
   return <div>Product {params.productId}</div>;
 }
