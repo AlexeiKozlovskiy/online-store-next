5ff9477f59bdff7cbe9940ce5bf577a82d973717
AlexeiKozlovskiy <gekleberefin@gmail.com>
2024-03-18 12:47:27 +0300

    fix: routing vercel, based on #8

--- my-app/src/app/not-found.tsx
+++ my-app/src/app/not-found.tsx
@@ -1,18 +1,24 @@
-import Link from 'next/link';
-import styles from '@/styles/not-found.module.scss';
+// import Link from 'next/link';
+// import styles from '@/styles/not-found.module.scss';
 
-export default function Custom404() {
-  return (
-    <div className={styles.page404__container}>
-      <p>Whoops!</p>
-      <p>Looks like you got lost.</p>
-      <p>
-        {`Go back to the `}
-        <Link className={styles.page404__link} href="/">
-          home
-        </Link>
-        {` page?`}
-      </p>
-    </div>
-  );
+// export default function Custom404() {
+//   return (
+//     <div className={styles.page404__container}>
+//       <p>Whoops!</p>
+//       <p>Looks like you got lost.</p>
+//       <p>
+//         {`Go back to the `}
+//         <Link className={styles.page404__link} href="/">
+//           home
+//         </Link>
+//         {` page?`}
+//       </p>
+//     </div>
+//   );
+// }
+
+import { redirect } from 'next/navigation';
+
+export default function NotFound() {
+  redirect('/not-found');
 }
